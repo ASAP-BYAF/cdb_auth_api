c98ef3ce3c0fb9e553068055128f3b6fcf0c6d8f
teppei <teppei0830.4869@gmail.com>
2023-11-23 21:22:21 +0900

    cors でアクセスを許可するホストを .env ファイルに書くようにしました。

--- api/main.py
+++ api/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routers import auth
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+# 環境変数を .env から読み込む。
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
+origin = os.environ.get("ALLOW_ORIGIN")
+origin = origin.split(",")
 
 app = FastAPI()
 app.include_router(auth.router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3000"],
+    allow_origins = origin,
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
